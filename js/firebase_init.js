9c6016870b57306e9bc12aa73a50851fcf2e6cbf
Nisha Bharti <rajeev15069@gmail.com>
2020-04-29 15:52:05 +0530

    Changes

---
+++ js/firebase_init.js
@@ -0,0 +1,6 @@
+//  The core Firebase JS SDK is always required and must be listed first
+include("https://www.gstatic.com/firebasejs/6.4.2/firebase-app.js");
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#config-web-app 
+include("https://www.gstatic.com/firebasejs/5.9.1/firebase-database.js");
--- js/mapping.js
+++ js/mapping.js
@@ -25,9 +25,28 @@ function getViewTypeName(cmpType) {
     return viewTypeMap.get(cmpType);
 }
 
+function bindViewTypeList(selectId) {
+    var list = document.getElementById(selectId);
+    for (var i in viewkeys) {
+        list.add(new Option(viewvalues[i], viewkeys[i]));
+    }
+}
+
+// Calculator mapping
+
 var calMap = new Map();
-var calKeys = [1, 2, 3];
-var calValues = ["EMI Calculator", "Compare Loan", "Flat Vs Reducing Rate"];
+var calKeys = [10, 20, 30,
+    40, 50, 60,
+    70, 80, 90,
+    100, 110, 120,
+    130, 140
+];
+var calValues = ["EMI Calculator", "Compare Loan", "Flat Vs Reducing Rate",
+    "Create Loan Profile", "View Loan Profile", "Home Loan Eligibility",
+    "Systematic Investment Plan", "Goal SIP Calculator", "Lumpsum SIP Calculator",
+    "FD Calculator", "RD Calculator", "PPF Calculator",
+    "GST Calculator", "VAT Calculator"
+];
 
 for (var i = 0; i < calKeys.length; i++) {
     calMap.set(calKeys[i], calValues[i]);
@@ -35,4 +54,51 @@ for (var i = 0; i < calKeys.length; i++) {
 
 function getCalTypeName(cmpType) {
     return calMap.get(cmpType);
+}
+
+function bindCalculatorList(selectId) {
+    var list = document.getElementById(selectId);
+    for (var i in calKeys) {
+        list.add(new Option(calValues[i], calKeys[i]));
+    }
+}
+
+// output type mapping 
+var outputType = new Map();
+var outputTypeKeys = [1, 2, 3];
+var outputTypeValues = ["Formulae", "Variable", "Fixed"];
+
+for (var i = 0; i < outputTypeKeys.length; i++) {
+    outputType.set(outputTypeKeys[i], outputTypeValues[i]);
+}
+
+function getOutPutTypeName(cmpType) {
+    return outputType.get(cmpType);
+}
+
+function bindOutputTypeList(selectId) {
+    var list = document.getElementById(selectId);
+    for (var i in outputTypeKeys) {
+        list.add(new Option(outputTypeValues[i], outputTypeKeys[i]));
+    }
+}
+
+//  Edittext Input type mapping 
+var etTypeMap = new Map();
+var etTypeKeys = [1, 2, 3];
+var etTypeValues = ["Number", "Decimal Number", "Text"];
+
+for (var i = 0; i < etTypeKeys.length; i++) {
+    etTypeMap.set(etTypeKeys[i], etTypeValues[i]);
+}
+
+function getetTypeName(cmpType) {
+    return etTypeMap.get(cmpType);
+}
+
+function bindetTypeList(selectId) {
+    var list = document.getElementById(selectId);
+    for (var i in etTypeKeys) {
+        list.add(new Option(etTypeValues[i], etTypeKeys[i]));
+    }
 }
